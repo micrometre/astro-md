989a707db1c5a7fe0462ebeb9b97cadd94e9d768
Henok <henokwehibe@gmail.com>
2024-06-26 22:54:51 +0100

    md adedd

---
+++ README.md
@@ -0,0 +1,1 @@
+# astro dynamic content with markdown
---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { z, defineCollection } from 'astro:content';
+
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    date: z.string(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
